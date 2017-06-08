ebda961327d108888981be42a8a8c9397ac5fd64
Cloogo <wang691234@gmail.com>
2017-06-08 12:14:42 +0800

    add bit ops

--- tutor/bitop/bitop.s
+++ tutor/bitop/bitop.s
@@ -1,10 +1,45 @@
-.data
-    num:
-
 .text
 .globl _start
 _start:
-    movabs $0x1000000000000000,%rbx
-    movb $2,%al
+    movabs $0xFFFFFFFFFFFFFFFF,%rbx
     sal %rbx
+    sar %rbx
 
+    shl %rbx
+    shr %rbx
+    shl %rbx
+
+    movb $2,%cl
+    sal %cl,%rbx
+    sar %cl,%rbx
+
+    shl %cl,%rbx
+    shr %cl,%rbx
+
+    movabs $0xFFFFFFFFFFFFFFFE,%rbx
+    rol %cl,%rbx
+    ror %cl,%rbx
+
+    rcl %cl,%rbx
+    rcr %cl,%rbx
+
+    clc
+    movabs $0xFFFFFFFFFFFFFFF4,%rbx
+    mov $0,%cl
+    bt %rcx,%rbx
+    mov $2,%cl
+    bt %rcx,%rbx
+
+    clc
+    movabs $0xFFFFFFFFFFFFFFF4,%rbx
+    mov $0,%cl
+    bts $0,%rbx
+    mov $1,%cl
+    btc %rcx,%rbx
+    mov $2,%cl
+    btr %rcx,%rbx
+
+    clc
+    movabs $0xFFFFFFFFFFFFFFF4,%rbx
+    bsf %rbx,%rax
+    bsr %rbx,%rbx
